e978c5aaccf5e95e0b9d0022f99e031ec994dff2
Xinhao.Yuan <xinhaoyuan@gmail.com>
2012-03-24 23:41:18 +0800

    .

---
+++ prj/common/lib/slab.h
@@ -0,0 +1,42 @@
+#ifndef __SLAB_H__
+#define __SLAB_H__
+
+#include <mach.h>
+#include <types.h>
+
+#define SLAB_META_SIZE sizeof(uintptr_t)
+#define SLAB_MIN_ALLOC 8
+#define SLAB_MIN_SHIFT 3
+#define SLAB_ALLOC_DELTA_SHIFT 10
+#define SLAB_MAX_SHIFT (SLAB_MIN_SHIFT + SLAB_ALLOC_DELTA_SHIFT)
+#define SLAB_MAX_ALLOC (SLAB_MIN_ALLOC << SLAB_ALLOC_DELTA_SHIFT)
+#define SLAB_MIN_BLOCK_PAGE 4
+#define SLAB_MAX_BLOCK_PAGE 256
+
+static char __slab_size_assert[(SLAB_MIN_BLOCK_PAGE << __PGSHIFT) >= SLAB_MAX_ALLOC ? 0 : -1] __attribute__((unused));
+
+struct slab_class_ctrl_s
+{
+	uint16_t alloc_size;
+	uint16_t block_size;
+	uintptr_t head;
+};
+
+typedef struct slab_class_ctrl_s slab_class_ctrl_s;
+typedef slab_class_ctrl_s *slab_class_ctrl_t;
+
+struct slab_ctrl_s
+{
+	void *(*page_alloc)(size_t num);
+	void  (*page_free)(void *addr);
+	slab_class_ctrl_s class_ctrls[SLAB_ALLOC_DELTA_SHIFT + 1];
+};
+
+typedef struct slab_ctrl_s slab_ctrl_s;
+typedef slab_ctrl_s *slab_ctrl_t;
+
+int   slab_init(slab_ctrl_t ctrl);
+void *slab_alloc(slab_ctrl_t slab, size_t size);
+void  slab_free(void *ptr);
+
+#endif
--- prj/kernel/model/malloc.c
+++ prj/kernel/model/malloc.c
@@ -6,114 +6,29 @@
 #include <spinlock.h>
 #include <arch/memlayout.h>
 #include <mmu.h>
+#include <lib/slab.h>
 
-#define EKL_META_SIZE 8
-#define EKL_MIN_ALLOC 8
-#define EKL_MIN_SHIFT 3
-#define EKL_ALLOC_DELTA_SHIFT 10
-#define EKL_MAX_SHIFT (EKL_MIN_SHIFT + EKL_ALLOC_DELTA_SHIFT)
-#define EKL_MAX_ALLOC (EKL_MIN_ALLOC << EKL_ALLOC_DELTA_SHIFT)
-#define EKL_MIN_BLOCK_PAGE 4
-#define EKL_MAX_BLOCK_PAGE 256
-
-static char __ekl_size_assert[(EKL_MIN_BLOCK_PAGE << PGSHIFT) >= EKL_MAX_ALLOC ? 0 : -1] __attribute__((unused));
-
-struct ekl_ctrl_s
-{
-	uint16_t alloc_size;
-	uint16_t block_size;
-	uintptr_t head;
-};
-
-
-struct mm_ctrl_s
-{
-	struct ekl_ctrl_s ekl_ctrls[EKL_ALLOC_DELTA_SHIFT + 1];
-};
-
-volatile struct mm_ctrl_s mm_ctrl;
-
-static int
-ekl_init(void)
-{
-	int i;
-	for (i = 0; i <= EKL_ALLOC_DELTA_SHIFT; ++ i)
-	{
-		mm_ctrl.ekl_ctrls[i].alloc_size = EKL_MIN_ALLOC << i;
-		mm_ctrl.ekl_ctrls[i].head = -1;
-		mm_ctrl.ekl_ctrls[i].block_size = EKL_MIN_BLOCK_PAGE;
-	}
-
-	return 0;
-}
-
-static void *
-ekl_ialloc(struct ekl_ctrl_s *ctrl)
-{
-	if (ctrl->head == (uintptr_t)-1)
-	{
-		size_t size = ctrl->block_size;
-		if (ctrl->block_size < EKL_MAX_BLOCK_PAGE)
-			ctrl->block_size <<= 1;
-		 
-		void *block = VADDR_DIRECT(PAGE_TO_PHYS(page_alloc_atomic(size)));
-		size <<= PGSHIFT;
-		 
-		if (block == NULL)
-		{
-			return NULL;
-		}
-		 
-		memset(block, 0, size);
-		 
-		uintptr_t *tail = (uintptr_t *)(
-			(uintptr_t)block +
-			((size / ctrl->alloc_size - 1) * (ctrl->alloc_size)));
-		*tail = -1;
-		 
-		ctrl->head = (uintptr_t)block;
-	}
-	 
-	uintptr_t *head = (uintptr_t *)ctrl->head;
-	ctrl->head = (*head == 0) ? (ctrl->head + ctrl->alloc_size) : (*head);
-	 
-	*head = (uintptr_t)ctrl;
-
-	return head + 1;
-}
-
-static void *
-ekl_alloc(size_t size)
-{
-	int size_index = BIT_SEARCH_LAST(size + EKL_META_SIZE - 1) - EKL_MIN_SHIFT + 1;
-	 
-	void *result;
-	if (size_index < 0 || size_index > EKL_ALLOC_DELTA_SHIFT)
-		result = NULL;
-	else result = ekl_ialloc((struct ekl_ctrl_s *)mm_ctrl.ekl_ctrls + size_index);
-
-	return result;
-}
-
-static void
-ekl_free(void *ptr)
-{
-	if (ptr == NULL) return;
-	 
-	uintptr_t *head = ((uintptr_t *)ptr - 1);
-	struct ekl_ctrl_s *ctrl = (struct ekl_ctrl_s *)*head;
-
-	*head = ctrl->head;
-	ctrl->head = (uintptr_t)head;
-}
-
+struct slab_ctrl_s slab_ctrl;
 spinlock_s malloc_lock;
+
+void *__slab_page_alloc(size_t num)
+{
+	return VADDR_DIRECT(PAGE_TO_PHYS(page_alloc_atomic(num)));
+}
+
+void __slab_page_free(void *addr)
+{
+	page_free_atomic(PHYS_TO_PAGE(PADDR_DIRECT(addr)));
+}
 
 int
 malloc_init(void)
 {
 	int result;
-	if ((result = ekl_init()) < 0) return result;
+	slab_ctrl.page_alloc = __slab_page_alloc;
+	slab_ctrl.page_free  = __slab_page_free;
+	
+	if ((result = slab_init(&slab_ctrl)) < 0) return result;
 	spinlock_init(&malloc_lock);
 
 	return 0;
@@ -130,12 +45,12 @@ kmalloc(size_t size)
 	int irq = irq_save();
 	spinlock_acquire(&malloc_lock);
 
-	if (size + EKL_META_SIZE > EKL_MAX_ALLOC)
+	if (size + SLAB_META_SIZE > SLAB_MAX_ALLOC)
 	{
 		size_t pagesize = (size + PGSIZE - 1) >> PGSHIFT;
 		result = VADDR_DIRECT(PAGE_TO_PHYS(page_alloc_atomic(pagesize)));
 	}
-	else result = ekl_alloc(size);
+	else result = slab_alloc(&slab_ctrl, size);
 
 	spinlock_release(&malloc_lock);
 	irq_restore(irq);
@@ -155,7 +70,7 @@ kfree(void *ptr)
 	{
 		page_free_atomic(PHYS_TO_PAGE(PADDR_DIRECT(ptr)));
 	}
-	else ekl_free(ptr);
+	else slab_free(ptr);
 
 	spinlock_release(&malloc_lock);
 	irq_restore(irq);
