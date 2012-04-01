4f416947817c220abb1b488136fd200796319fc0
Xinhao.Yuan <xinhaoyuan@gmail.com>
2012-04-01 21:19:13 +0800

    .

--- prj/kernel/model/entry.c
+++ prj/kernel/model/entry.c
@@ -15,14 +15,12 @@
 void
 kernel_start(void)
 {
+	/* Some kernel-user component initializations here */
 	mbox_init();
 	mbox_irq_init();
 	kern_lwip_init();
 	nic_init();
 
-	/* proc_init(&p, ".nic", SCHED_CLASS_RR, (void(*)(void *))test, NULL, (uintptr_t)ARCH_STACKTOP(p_stack, 10240)); */
-	/* proc_notify(&p); */
-	
 	/* Load the user init image */
 	
 	extern char _binary_user_init_image_start[];
